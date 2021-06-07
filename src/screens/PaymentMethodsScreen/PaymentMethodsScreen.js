4a1884706176f1b39af299f1321065f2bc18fa01
Đàm Minh Phúc <huyenche123@gmail.com>
2021-06-07 17:33:06 +0700

    PaymentMethodsScreen and UserScreen

---
+++ src/screens/PaymentMethodsScreen/PaymentMethodsScreen.js
@@ -0,0 +1,101 @@
+import React, {useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import {theme} from '../../common/theme';
+
+const PaymentMethodsScreen = () => {
+  const DATA = [
+    {
+      id: '9',
+      title: 'Cash',
+      selected: false,
+    },
+    {
+      id: '10',
+      title: 'Airpay',
+      selected: true,
+    },
+    {
+      id: '11',
+      title: 'Momo',
+      selected: false,
+    },
+    {
+      id: '12',
+      title: 'VNPay',
+      selected: false,
+    },
+    {
+      id: '13',
+      title: 'ViettelPay',
+      selected: false,
+    },
+    {
+      id: '14',
+      title: 'VinaPay',
+      selected: false,
+    },
+  ];
+  return (
+    <View style={styles.container}>
+      <Picker
+        style={styles.pickerStyle}
+        data={DATA}
+        optionStyle={styles.optionStyle}
+        selectedOptionStyle={styles.selectedOptionStyle}
+        optionTextStyle={styles.optionTextStyle}
+        selectedOptionTextStyle={styles.selectedOptionTextStyle}
+      />
+      <View>
+        <TouchableOpacity
+          style={{
+            alignSelf: 'flex-end',
+            backgroundColor: theme.colors.DARK,
+          }}
+          onPress={() =>
+            navigation.navigate('AddPayCardScreen')
+          }></TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 100,
+  },
+  optionStyle: {
+    margin: 5,
+    width: 100,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    backgroundColor: '#E9EFFE',
+    fontFamily: 'Helevica Neue',
+  },
+  selectedOptionStyle: {
+    margin: 5,
+    width: 100,
+    height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    backgroundColor: '#497DF9',
+    fontFamily: 'Helevica Neue',
+  },
+  optionTextStyle: {
+    color: '#497DF9',
+  },
+  selectedOptionTextStyle: {
+    color: '#ffffff',
+  },
+});
+
+export default PaymentMethodsScreen;
--- src/screens/UserScreen/UserScreen.js
+++ src/screens/UserScreen/UserScreen.js
@@ -1,20 +1,176 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import {logoutUser} from '../../api/auth';
+import React, {useEffect} from 'react';
+import {
+  StyleSheet,
+  View,
+  StatusBar,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
-export default function UserScreen() {
+import {theme} from '../../common/theme';
+import {CustomText} from '../../components/CustomText';
+import {logoutUser} from '../../api/auth';
+import {connect} from 'react-redux';
+import {getCurrentUserData, selectUserData} from '../../store/users';
+import {selectCurrentProduct} from '../../store/products';
+import {GLOBAL_STYLES} from '../../common/globalStyles';
+const mapStateToProps = state => ({
+  user: selectUserData(state),
+  product: selectCurrentProduct(state),
+});
+
+export const ProfileScreen = connect(mapStateToProps, {
+  getCurrentUserData,
+  logoutUser,
+})(({getCurrentUserData, navigation, user}) => {
+  const handleGetCurrentUserData = async () => {
+    try {
+      await getCurrentUserData();
+    } catch (error) {
+      console.log('getNewData', error);
+    }
+  };
+
+  useEffect(() => {
+    handleGetCurrentUserData();
+  }, []);
+
+  const profileSections = [
+    {
+      sectionName: 'My Orders',
+      dutyOfSection: `Already have ${user.orders.length} orders`,
+      screenTo: 'MyOrders',
+    },
+    {
+      sectionName: 'Shipping addresses',
+      dutyOfSection: `${(user.shippingAddresses || []).length} addresses`,
+      screenTo: 'ShippingAddressesScreen',
+    },
+    {
+      sectionName: 'Payment methods',
+      dutyOfSection: `${(user.paymentMethods || []).length} payment methods `,
+      screenTo: 'PaymentMethods',
+    },
+    {
+      sectionName: 'Settings',
+      dutyOfSection: 'Change photo and username',
+      screenTo: 'Settings',
+    },
+  ];
   return (
     <View style={styles.container}>
-      <Text> Hello </Text>
-      <TouchableOpacity onPress={logoutUser}>
-        <Text>LogOut</Text>
-      </TouchableOpacity>
+      <StatusBar />
+      <View style={styles.logoutWrapper}>
+        <CustomText weight={'bold'} style={styles.title}>
+          My Profile
+        </CustomText>
+      </View>
+      <View style={styles.userInfoSection}>
+        <Image
+          style={styles.avatar}
+          source={require('../../assets/images/Rose_2.png')}
+        />
+        <View style={styles.text}>
+          <CustomText weight={'bold'} style={styles.name}>
+            {user.username}
+          </CustomText>
+          <CustomText weight={'medium'} style={styles.email}>
+            {user.email}
+          </CustomText>
+        </View>
+      </View>
+      <FlatList
+        data={profileSections}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={styles.profileSection}
+            key={`${item.sectionName}`}
+            onPress={() => navigation.navigate(item.screenTo)}>
+            <View style={styles.text}>
+              <CustomText weight={'bold'} style={styles.sectionName}>
+                {item.sectionName}
+              </CustomText>
+              <CustomText style={styles.sectionDesc}>
+                {item.dutyOfSection}
+              </CustomText>
+            </View>
+          </TouchableOpacity>
+        )}
+        keyExtractor={item => item.sectionName}
+      />
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: theme.colors.background,
+    paddingHorizontal: GLOBAL_STYLES.PADDING,
+  },
+  title: {
+    color: theme.colors.TEXT,
+    fontSize: 34,
+    marginVertical: 24,
+  },
+  email: {
+    color: theme.colors.GRAY,
+    fontSize: 16,
+    lineHeight: 20,
+    marginBottom: 15,
+    marginLeft: 30,
+  },
+  name: {
+    color: theme.colors.TEXT,
+    fontSize: 20,
+    lineHeight: 22,
+    marginLeft: 30,
+    marginBottom: 8,
+  },
+
+  sectionName: {
+    color: theme.colors.TEXT,
+    fontSize: 18,
+    marginBottom: 8,
+  },
+
+  sectionDesc: {
+    color: theme.colors.GRAY,
+    fontSize: 14,
+  },
+
+  profileSection: {
+    width: '100%',
+    height: 72,
+    borderBottomWidth: 0.5,
+    borderColor: theme.colors.GRAY,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  text: {
+    marginTop: 10,
+  },
+  userInfoSection: {
+    width: '100%',
+    height: 120,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+
+  avatar: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+    backgroundColor: 'white',
+  },
+
+  logoutWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingRight: 16,
   },
 });
